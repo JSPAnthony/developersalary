bcd3c3ebe6b89b145938927f8f6fcf0ffeebf2af
Justice Sitsofe Anthony <jspanthony@gmail.com>
2022-09-29 16:22:01 +0000

    tkinter removed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from enum import auto
-from tkinter import Image
+# from enum import auto
+# from tkinter import Image
 import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
